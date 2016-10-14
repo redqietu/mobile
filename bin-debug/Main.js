8670808427d8bfca8d9ee82da893527634d40d0f
wanghongxin492 <wanghongxin492@emao.com>
2016-10-14 20:09:37 +0800

    测试tween

---
+++ bin-debug/Main.js
@@ -0,0 +1,191 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-present, Egret Technology.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+var Main = (function (_super) {
+    __extends(Main, _super);
+    function Main() {
+        _super.call(this);
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+    }
+    var d = __define,c=Main,p=c.prototype;
+    p.onAddToStage = function (event) {
+        //设置加载进度界面
+        //Config to load process interface
+        this.loadingView = new LoadingUI();
+        this.stage.addChild(this.loadingView);
+        //初始化Resource资源加载库
+        //initiate Resource loading library
+        RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.loadConfig("resource/default.res.json", "resource/");
+    };
+    /**
+     * 配置文件加载完成,开始预加载preload资源组。
+     * configuration file loading is completed, start to pre-load the preload resource group
+     */
+    p.onConfigComplete = function (event) {
+        RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+        RES.addEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
+        RES.loadGroup("preload");
+    };
+    /**
+     * preload资源组加载完成
+     * Preload resource group is loaded
+     */
+    p.onResourceLoadComplete = function (event) {
+        if (event.groupName == "preload") {
+            this.stage.removeChild(this.loadingView);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+            RES.removeEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
+            this.createGameScene();
+        }
+    };
+    /**
+     * 资源组加载出错
+     *  The resource group loading failed
+     */
+    p.onItemLoadError = function (event) {
+        console.warn("Url:" + event.resItem.url + " has failed to load");
+    };
+    /**
+     * 资源组加载出错
+     *  The resource group loading failed
+     */
+    p.onResourceLoadError = function (event) {
+        //TODO
+        console.warn("Group:" + event.groupName + " has failed to load");
+        //忽略加载失败的项目
+        //Ignore the loading failed projects
+        this.onResourceLoadComplete(event);
+    };
+    /**
+     * preload资源组加载进度
+     * Loading process of preload resource group
+     */
+    p.onResourceProgress = function (event) {
+        if (event.groupName == "preload") {
+            this.loadingView.setProgress(event.itemsLoaded, event.itemsTotal);
+        }
+    };
+    /**
+     * 创建游戏场景
+     * Create a game scene
+     */
+    p.createGameScene = function () {
+        this.createGameScene1();
+    };
+    p.createGameScene1 = function () {
+        var scene = new egret.DisplayObjectContainer;
+        var bg0 = new egret.Shape;
+        var stageW = this.stage.stageWidth;
+        var stageH = this.stage.stageHeight;
+        bg0.graphics.beginFill(0xffda0e, 1);
+        bg0.graphics.drawRect(0, 0, stageW, stageH);
+        bg0.graphics.endFill();
+        scene.addChild(bg0);
+        var bg1 = this.createBitmapByName("bk-sanshe_png");
+        scene.addChild(bg1);
+        var bg2 = this.createBitmapByName("bk-dian_png");
+        scene.addChild(bg2);
+        var bg3 = this.createBitmapByName("bk-p1_png");
+        scene.addChild(bg3);
+        var bgL = this.createBitmapByName('left_png');
+        var bgR = this.createBitmapByName('right_png');
+        bgL.x = 0;
+        bgL.y = 658;
+        scene.addChild(bgL);
+        bgR.x = stageW - 141;
+        bgR.y = 658;
+        scene.addChild(bgR);
+        var logo = this.createBitmapByName('icon-emao_png');
+        scene.addChild(logo);
+        var btn = this.createBitmapByName('btn-lijixiaheishou_png');
+        scene.addChild(btn);
+        btn.x = 82;
+        btn.y = 995;
+        this.addChild(scene);
+        btn.touchEnabled = true;
+        btn.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            window['scene'] = scene;
+        }, this, true);
+    };
+    p.createGameScene2 = function () {
+    };
+    /**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
+    p.createBitmapByName = function (name) {
+        var result = new egret.Bitmap();
+        var texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    };
+    /**
+     * 描述文件加载成功，开始播放动画
+     * Description file loading is successful, start to play the animation
+     */
+    p.startAnimation = function (result) {
+        var self = this;
+        var parser = new egret.HtmlTextParser();
+        var textflowArr = [];
+        for (var i = 0; i < result.length; i++) {
+            textflowArr.push(parser.parser(result[i]));
+        }
+        var textfield = self.textfield;
+        var count = -1;
+        var change = function () {
+            count++;
+            if (count >= textflowArr.length) {
+                count = 0;
+            }
+            var lineArr = textflowArr[count];
+            self.changeDescription(textfield, lineArr);
+            var tw = egret.Tween.get(textfield);
+            tw.to({ "alpha": 1 }, 200);
+            tw.wait(2000);
+            tw.to({ "alpha": 0 }, 200);
+            tw.call(change, self);
+        };
+        change();
+    };
+    /**
+     * 切换描述内容
+     * Switch to described content
+     */
+    p.changeDescription = function (textfield, textFlow) {
+        textfield.textFlow = textFlow;
+    };
+    return Main;
+}(egret.DisplayObjectContainer));
+egret.registerClass(Main,'Main');
+//# sourceMappingURL=Main.js.map
