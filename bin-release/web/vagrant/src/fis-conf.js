cf110af76eabd4a86c9645954de90c6585566c64
wanghongxin492 <wanghongxin492@emao.com>
2017-03-03 15:20:59 +0800

    taohuayun

--- bin-release/web/vagrant/src/fis-conf.js
+++ bin-release/web/vagrant/src/fis-conf.js
@@ -1,37 +1,11 @@
-fis.config.set('project.watch.usePolling', true);
-
-
-//前端同学开发完自测没问题后，执行此命令,产出线上代码
 fis.media('build')
 .hook('relative')
-// fis3-hook-relative 插件进行压缩，已内置
 .match('**', {
   relative: true
 })
 .match('*', {
-    //对资源引用进行转换
-    // domain: ''
-})
-
-
-//配置共用资源单独打包
-// .match('/libs/**.js', {
-//  packTo: '/pkg/libs.js'
-// })
-
-
-// fis-optimizer-uglify-js 插件进行压缩，已内置
-//配置文本替换
-// .match('**', {
-//     deploy: [
-//         fis.plugin('replace', {
-//             from: /http:\/\/adms\.emao\.com\/fl\/getadcs/,
-//             to: '/test/data'
-//         }),
-//         fis.plugin('local-deliver')
-//     ]
-// })
-.match('::package', {
+    // domain: 'http://zt.m.emao.com/201607/Q2bps'
+}).match('::package', {
     postpackager: fis.plugin('loader', {
         allInOne: true
     })
@@ -45,25 +19,16 @@ fis.media('build')
     // fis-optimizer-png-compressor 插件进行压缩，已内置
     optimizer: fis.plugin('png-compressor')
 }).match('*.{png,jpg,gif,svg}}', {
-    // fis3-optimizer-imagemin 插件进行压缩，已内置
   optimizer: fis.plugin('imagemin', {
-                            ".png": {
-                                pngquant: {
-                                    quality: '65-80',
-                                    speed: 1,
-                                }
-                            }
-                        })
-}).match('*.{js,css}',{
-    useHash:true,
-    useSprite:true,
-    // optimizer:true
-}).match('*.{png}',{
-    // useHash:true,
-    // useSprite:true,
-    // optimizer:true
-}).match('*', {
-    //fis3-postpackager-loader 插件进行压缩，已内置
+  ".png": {
+    pngquant: {
+      quality: '65-80',
+      speed: 1,
+    }
+  }
+})
+}).
+match('*', {
     deploy: fis.plugin('local-deliver', {
         to: '../build'
     })
